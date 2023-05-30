10473ddf5cfe33e9f9b83b3a0e97ab1f139b39cd
Gautham Vanjre <vanjregautham@gmail.com>
2023-05-30 18:46:44 +0530

    added to optionally select to enter recommended

--- templates/index.js
+++ templates/index.js
@@ -4,16 +4,24 @@ let button = document.getElementsByClassName("btn");
 let current = document.querySelector(".output").innerHTML;
 let recommended = document.querySelectorAll(".output")[1].innerHTML;
 let profit = document.querySelectorAll(".output")[2];
-
-console.log("Hello World");
-console.log(current);
-console.log(recommended);
+let yesButton = document.getElementById("flexRadioDefault1");
+let noButton = document.getElementById("flexRadioDefault2");
+let recommendedSelect = document.getElementById("recommendedSelect");
+let radioBtn = document.getElementById("radioBtn-container");
 
 profit.innerHTML = ((recommended - current) / recommended) * 100;
 
-// button.Evc("click", () => {
-//   Console.log("Hello World");
-// });
+function togglePara() {
+  if (yesButton.checked) {
+    recommendedSelect.classList.remove("hidden");
+
+    radioBtn.classList.add("hidden");
+    console.log(radioBtn.classList);
+  } else {
+    recommendedSelect.classList.add("hidden");
+    console.log("Hello World2");
+  }
+}
 
 button[0].addEventListener("click", () => {
   console.log("Hello World");
